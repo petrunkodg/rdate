66f0210b6a30d2c50423c5a1f30098695ccff050
Petrunko D <d.petrunko@corp.mail.ru>
2020-08-29 19:06:50 +0300

    [license] copyrights

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright © 2020 Danila Petrunko. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 /*
 rdate package implements a few primitives to work with time and periods conveniently.
 It's especially useful for creating reports.
